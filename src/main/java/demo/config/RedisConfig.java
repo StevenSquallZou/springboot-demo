0a9483ff2caef4eb28242ef86fb2c718ab5ee7f5
Steven Squall <zoukailiang0@163.com>
2025-07-27 20:47:22 +0800

    add printRedisConfig

--- src/main/java/demo/config/RedisConfig.java
+++ src/main/java/demo/config/RedisConfig.java
@@ -1,19 +1,24 @@
 package demo.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
+@Slf4j
 public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        printRedisConfig(redisConnectionFactory);
+
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
         redisTemplate.setConnectionFactory(redisConnectionFactory);
@@ -23,6 +28,19 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
+    }
+
+
+    private void printRedisConfig(RedisConnectionFactory redisConnectionFactory) {
+        // Print Redis connection details
+        if (redisConnectionFactory instanceof LettuceConnectionFactory) {
+            LettuceConnectionFactory lettuceFactory = (LettuceConnectionFactory) redisConnectionFactory;
+            String host = lettuceFactory.getHostName();
+            int port = lettuceFactory.getPort();
+            log.info("Redis Connection - Host: {}, Port: {}", host, port);
+        } else {
+            log.info("Using RedisConnectionFactory of type: {}", redisConnectionFactory.getClass().getName());
+        }
     }
 
 }
