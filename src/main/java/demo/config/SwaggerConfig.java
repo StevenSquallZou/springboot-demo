5469f75881846e99e64e0755b569ce873996df76
Steven Squall <zoukailiang0@163.com>
2025-08-03 18:49:25 +0800

    Overriding swagger server url

--- src/main/java/demo/config/SwaggerConfig.java
+++ src/main/java/demo/config/SwaggerConfig.java
@@ -3,12 +3,15 @@ package demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@Slf4j
 public class SwaggerConfig {
     @Value("${APP_VERSION}")
     protected String appVersion;
@@ -21,7 +24,12 @@ public class SwaggerConfig {
         info.setVersion(appVersion);
         info.setDescription("SpringBoot-Demo API specifications");
 
-        return new OpenAPI().info(info);
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setInfo(info);
+        openAPI.addServersItem(new Server().url("http://springboot-demo.com:30080"));
+        log.info("Overriding server url to http://springboot-demo.com:30080");
+
+        return openAPI;
     }
 
 }
