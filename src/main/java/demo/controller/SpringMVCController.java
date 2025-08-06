41a00ae4eb1cbd4be6eca1d45a3c2ede910f9a8a
Steven Squall <zoukailiang0@163.com>
2025-08-06 12:22:50 +0800

    add http get parameter for Spring MVC sample

--- src/main/java/demo/controller/SpringMVCController.java
+++ src/main/java/demo/controller/SpringMVCController.java
@@ -1,23 +1,27 @@
 package demo.controller;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @RequestMapping("/test")
+@Validated
 @Slf4j
 public class SpringMVCController {
 
     @GetMapping
-    public String test(Model model) {
+    public String test(@RequestParam(name = "message") @NotBlank String message, Model model) {
         log.info("test method called");
 
-        model.addAttribute("message", "This is a test message for Spring MVC");
+        model.addAttribute("message", message);
 
         // This will resolve to a view named "test.html" or "test.jsp" depending on your view resolver configuration
         return "test";
