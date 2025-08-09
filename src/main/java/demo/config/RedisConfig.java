a03ef36a86aba20f13f746731a43bab77e54567e
Steven Squall <zoukailiang0@163.com>
2025-08-09 12:21:55 +0800

    get configs from config server

--- src/main/java/demo/config/RedisConfig.java
+++ src/main/java/demo/config/RedisConfig.java
@@ -2,8 +2,10 @@ package demo.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,8 +14,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
+@RefreshScope
 @Slf4j
 public class RedisConfig {
+/*    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;*/
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -36,7 +45,8 @@ public class RedisConfig {
         if (redisConnectionFactory instanceof LettuceConnectionFactory lettuceFactory) {
             String host = lettuceFactory.getHostName();
             int port = lettuceFactory.getPort();
-            log.info("Redis Connection - Host: {}, Port: {}", host, port);
+            log.info("Redis Connection - host: {}, port: {}", host, port);
+//            log.info("Redis Connection - configured host: {}, port: {}", redisHost, redisPort);
         } else {
             log.info("Using RedisConnectionFactory of type: {}", redisConnectionFactory.getClass().getName());
         }
